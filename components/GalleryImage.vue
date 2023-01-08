6b9d96fa22defe854c6468a9a0fff42daba80b9e
lharm <lharm@lynet.de>
2023-01-08 05:31:21 +0100

    minor layout fix for gallery

--- components/GalleryImage.vue
+++ components/GalleryImage.vue
@@ -10,16 +10,16 @@
     </div>
     <div class="image-cell-tail d-flex flex-row">
       <div class="d-flex flex-column flex-wrap">
-         <strong style="font-size: smaller" class="">
-           #{{prompt}}
-         </strong>
+        <strong style="font-size: smaller" class="">
+          #{{ prompt }}
+        </strong>
         <div>
-          Von: {{username}}
+          Von: {{ username }}
         </div>
       </div>
       <div class="d-flex flex-column flex-fill">
         <div>
-          
+
         </div>
       </div>
     </div>
@@ -44,19 +44,22 @@ export default {
   },
   methods:
       {
-        waitForData: async function()
-        {
-          const user =  await helper.getUserById(this.image.user)
+        waitForData: async function () {
+          const user = await helper.getUserById(this.image.user)
           const prompt = await helper.getPromptById(this.image.prompt)
           this.username = user.username
           this.prompt = prompt.prompt_text.toUpperCase()
-         
+        },
+        insertBlankCharToImageName: function () {
+          if (typeof this.image.name == "undefined" || this.image.name === "") {
+            this.image.name = '\u00A0'
+          }
         }
       },
   computed:
       {
         image_path() {
-          return "background-image:url('" +  this.api_url + this.image.collectionId + '/' + this.image.id + '/' + this.image.photo_file + "')"
+          return "background-image:url('" + this.api_url + this.image.collectionId + '/' + this.image.id + '/' + this.image.photo_file + "')"
         },
       },
   props: {
@@ -64,6 +67,7 @@ export default {
   },
   mounted() {
     this.waitForData()
+    this.insertBlankCharToImageName()
   }
 }
 
