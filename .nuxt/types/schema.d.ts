00d87596c18c561b2ece15d8aac4fd7a362995c2
lharm <lharm@lynet.de>
2023-01-19 01:52:09 +0100

    updated auth middleware

--- .nuxt/types/schema.d.ts
+++ .nuxt/types/schema.d.ts
@@ -1,7 +1,9 @@
 import { NuxtModule } from '@nuxt/schema'
 declare module '@nuxt/schema' {
   interface NuxtConfig {
+    ["pwa"]?: typeof import("@kevinmarrec/nuxt-pwa").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
     ["icon"]?: typeof import("nuxt-icon").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    ["nuxt-config-schema"]?: typeof import("nuxt-config-schema").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
     ["pinia"]?: typeof import("@pinia/nuxt").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
     ["telemetry"]?: typeof import("@nuxt/telemetry").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
   }
@@ -15,6 +17,24 @@ declare module '@nuxt/schema' {
    },
   }
   interface PublicRuntimeConfig {
+   pwaManifest: {
+      name: string,
 
+      short_name: any,
+
+      description: any,
+
+      lang: string,
+
+      start_url: string,
+
+      display: string,
+
+      background_color: string,
+
+      theme_color: string,
+
+      icons: Array<any>,
+   },
   }
 }
